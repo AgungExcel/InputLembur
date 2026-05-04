d526bc83d4e5a8507009ca9520f2163066f242ef
AgungExcel <67411110+AgungExcel@users.noreply.github.com>
2026-05-04 20:42:34 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -6,7 +6,7 @@
 // Catatan: anon key memang public untuk aplikasi browser. Keamanan data dikontrol oleh RLS/policies di Supabase.
 window.HOPLUN_CONFIG = {
   SUPABASE_URL: "https://xdylrhneskltftzvamtt.supabase.co/rest/v1/",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhkeWxyaG5lc2tsdGZ0enZhbXR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc4ODk5NzEsImV4cCI6MjA5MzQ2NTk3MX0.Wgx8612N_zgoFD_z07cfLEW83yfydmMiVPGxCGzhiXU",
+  SUPABASE_ANON_KEY: "sb_publishable_E3ijgEkYofhGw21DF34aBA_PA3IvndC",
   USE_SUPABASE: true,
   APP_NAME: "Aplikasi Input Lembur",
   COMPANY_NAME: "PT Hop Lun Indonesia"
