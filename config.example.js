29480892368aa9fff9c243d40a68cee3cff525ac
AgungExcel <67411110+AgungExcel@users.noreply.github.com>
2026-05-04 20:42:53 +0700

    Delete config.example.js

--- config.example.js
+++
@@ -1,7 +0,0 @@
-window.HOPLUN_CONFIG = {
-  SUPABASE_URL: "https://xxxxxxxxxxxxxxxxxxxx.supabase.co",
-  SUPABASE_ANON_KEY: "paste-anon-key-di-sini",
-  USE_SUPABASE: true,
-  APP_NAME: "Aplikasi Input Lembur",
-  COMPANY_NAME: "PT Hop Lun Indonesia"
-};
